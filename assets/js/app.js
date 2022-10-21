b9b3a87c1244ac6831fe9ab1a068edaadbd88480
ahmed <ahmed.nageeb.mahmoud.hssan@gmail.com>
2022-10-21 23:08:57 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,27 +3,33 @@ const ngApp = angular.module("ngApp", []);
 ngApp.controller("myCtrl", [
   "$scope",
   function (s) {
+    s.supportedLanguages=SupportedLanguages;
+    s.supportedLanguagesSize=SupportedLanguages.length;
     s.config = {
-      addAllSubtitleLanguagesSupportedByYoutubeAndTubebuddyOnly: true,
-      translateTitleAndDescriptionAutomaticallyByTubebuddyExtension: true,
-      automaticallyTranslateSubtitleFiles: true,
+      isAllowAddLanguages: true,
+      isAllowTranslateTitle: true,
+      isAllowAddSubtitle:false
     };
 
     s.buildScript = () => {
-      s.script = `
-        var script = document.createElement('script');
-        script.src="${window.location.href}assets/js/youtubeScript.js";
-        script.id="ahmedsezeryoutubescript";
-        script.setAttribute("skipNotSupportedLanguages",${s.config.addAllSubtitleLanguagesSupportedByYoutubeAndTubebuddyOnly})
-        script.setAttribute("isAllowTranslateTitle",${s.config.translateTitleAndDescriptionAutomaticallyByTubebuddyExtension})
-        script.setAttribute("isAllowAddSubtitle",${s.config.automaticallyTranslateSubtitleFiles})
-        document.body.appendChild(script);
-`;
+      let cllScript = `
+        var youTubeScriptCallElemtn=document.createElement("script")
+        youTubeScriptCallElemtn.innerHTML=\`var youtubeScriptTranslationObj = new YoutubeScriptTranslation();
+        youtubeScriptTranslationObj.isAllowAddLanguages = ${s.config.isAllowAddLanguages};
+        youtubeScriptTranslationObj.isAllowTranslateTitle = ${s.config.isAllowTranslateTitle};
+        youtubeScriptTranslationObj.isAllowAddSubtitle = ${s.config.isAllowAddSubtitle};
+        youtubeScriptTranslationObj.addLanguages()\`
+        document.body.appendChild(cllScript);
+      `;
 
- 
+      s.script = `
+        var youtubeScript = document.createElement('script');
+        youtubeScript.src="${window.location.href}/assets/js/youtubeScript.js";
+        document.body.appendChild(youtubeScript);
+        ${cllScript}
+`;
     };
 
     s.buildScript();
   },
 ]);
-
